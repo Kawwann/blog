117c542e7de675c1301da3a0637f9ba0d5e7e7aa
Kawann <kawan.romao.costa@escola.pr.gov.br>
2026-06-30 07:39:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function changeFont(font){
 
+    document.body.style.fontFamily = font;
+
+}
+
+function scrollToSection(id){
+
+    document.getElementById(id).scrollIntoView({
+
+        behavior:"smooth"
+
+    });
+
+}
+
+const cards = document.querySelectorAll(".iphone-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        card.classList.toggle("selected");
+
+    });
+
+});
